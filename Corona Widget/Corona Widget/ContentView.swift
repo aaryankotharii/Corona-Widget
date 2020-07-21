4f91440368c479ea9ad4a937b8546339811063e6
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-21 23:59:36 +0530

    initial commit

---
+++ Corona Widget/Corona Widget/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Corona Widget
+//
+//  Created by Aaryan Kothari on 21/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Corona Widget/Corona Widget/Corona_WidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Corona_WidgetApp.swift
+//  Corona Widget
+//
+//  Created by Aaryan Kothari on 21/07/20.
+//
+
+import SwiftUI
+
+@main
+struct Corona_WidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
