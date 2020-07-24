7fa2752d3e1abd29305f60fb4b3296480a10acd0
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-25 00:44:15 +0530

    ContentView basic setup

--- Corona Widget/Corona Widget/View/ContentView.swift
+++ Corona Widget/Corona Widget/View/ContentView.swift
@@ -10,12 +10,18 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var corona = SessionStore()
     var body: some View {
-        Text("\(corona.current?.Global.TotalDeaths ?? 0)").padding()
-            .onAppear {
-                corona.fetch{ idk in
-                    
-                }
-            }
+        VStack{
+            Text("Hey There! 👋")
+                .font(.system(size: 100, weight: .regular, design: .rounded))
+            Text("\(corona.current?.Global.TotalConfirmed ?? 0)").padding()
+        }.edgesIgnoringSafeArea(.all)
+        .background(LinearGradient(gradient: Gradient(colors: [.white, Color(#colorLiteral(red: 0.631372549, green: 0.6, blue: 1, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+        
+        .onAppear(perform: fetch)
+        
+    }
+    func fetch(){
+        corona.fetch{ data in }
     }
 }
 
