cf923f86fe156c3e33d379979890a8aedbbc1974
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-24 11:22:17 +0530

    emojiConverter added

---
+++ Corona Widget/Corona Widget/emojiConverter.swift
@@ -0,0 +1,15 @@
+//
+//  emojiConverter.swift
+//  Corona Widget
+//
+//  Created by Aaryan Kothari on 24/07/20.
+//
+
+import Foundation
+
+func convertToEmoji(str: String) -> String {
+    let lowercased = str.lowercased()
+    guard lowercased.count == 2 else { return "" }
+    let regionalIndicators = lowercased.unicodeScalars.map { UnicodeScalar($0.value + (0x1F1E6 - 0x61))! }
+    return String(regionalIndicators.map { Character($0) })
+}
