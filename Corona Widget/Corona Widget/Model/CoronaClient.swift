ddaf1d6cf9b81a38bd67c3dfc7d537795f9b8949
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-24 21:32:03 +0530

    refactor: CoronaClient

--- Corona Widget/Corona Widget/Model/CoronaClient.swift
+++ Corona Widget/Corona Widget/Model/CoronaClient.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+// MARK:- NETWORKING CLASS
+
+// Endoint to fetch stats on Cobid-19
 private let url = URL(string: "https://api.covid19api.com/summary")
 
+
+// JSON decoder used to comvert the fetched json into a struct
 private var decoder: JSONDecoder{
     let decode = JSONDecoder()
     decode.keyDecodingStrategy = .convertFromSnakeCase
@@ -16,21 +21,22 @@ private var decoder: JSONDecoder{
 }
 
 class CoronaClient {
-        class func fetchSummary(onSuccess: @escaping (Corona) -> Void){
-            guard let url = url else { return }
-    
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                guard let data = data, error == nil else{  return  }
-                do{
-                    let coronaData = try decoder.decode(Corona.self, from: data)
-                    DispatchQueue.main.async {
-                        onSuccess(coronaData)
-                    }
-                }catch{
-                    print(error.localizedDescription)
+    // function to fetch covid-19 stats from `url`
+    class func fetchSummary(onSuccess: @escaping (Corona) -> Void){
+        guard let url = url else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else{  return  }
+            do{
+                let coronaData = try decoder.decode(Corona.self, from: data)
+                DispatchQueue.main.async {
+                    onSuccess(coronaData)
                 }
-            }.resume()
-        }
+            }catch{
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
 }
 
 
