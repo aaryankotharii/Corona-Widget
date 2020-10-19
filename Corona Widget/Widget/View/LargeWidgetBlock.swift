c3cb872006e5a0c5f811518fccf8b0ddbcbd3e02
Devanshi Modha <devanshimodha@yahoo.com>
2020-10-20 01:27:21 +0530

    Fix large widget padding (#10)
    * Fix Padding
    
    * fix padding issue
    
    Co-authored-by: devanshim <devanshim@hyperlinkinfosystem.net.in>

--- Corona Widget/Widget/View/LargeWidgetBlock.swift
+++ Corona Widget/Widget/View/LargeWidgetBlock.swift
@@ -13,14 +13,17 @@ struct LargeWidgetBlock: View {
     
     // MARK - BODY
     var body: some View {
-        VStack{
+        VStack(alignment: .leading, spacing: 5){
             VStack(alignment:.leading){
                 countStack(total: data.Global.TotalConfirmed,color: .coronapink, name: "confirmed")
+                Spacer()
                 countStack(total: data.Global.TotalRecovered,  color: .coronagreen, name: "recovered")
+                Spacer()
                 countStack(total: data.Global.TotalDeaths,color: .coronagrey, name: "deaths")
+                Spacer()
                 countStack(total: totalActive(),color: .coronayellow, name: "active")
             } //: VSTACK
-            VStack(alignment:.leading){
+            VStack(alignment:.leading, spacing: 3){
                 countStack(total: data.Global.NewConfirmed,color: .coronapink, name: "confirmed",isActive: true,type: .total)
                 countStack(total: data.Global.NewRecovered, color: .coronagreen, name: "recovered",isActive: true,type: .recovered)
                 countStack(total: data.Global.NewDeaths,color: .coronagrey, name: "deaths",isActive: true,type: .deaths)
